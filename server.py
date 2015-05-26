820f9e23ffbf9d20aef4761695b73cbc1b42d4af
BING <407272759@qq.com>
2015-05-26 18:39:32 +0800

    使用socket发送和接受简单的数据�

---
+++ server.py
@@ -0,0 +1,9 @@
+import socket
+port = 8081
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.bind(("",port))
+print "Waiting on port:",port
+while True:
+    data, addr = s.recvfrom(1024)
+    print "Received:",data,"from",addr
