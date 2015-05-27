ad3aa6471f95eac477e1d54cda9f9c9f6be52c70
BING <407272759@qq.com>
2015-05-27 22:28:04 +0800

    用mysql连接数据库

---
+++ usemysql.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+conn = mysql.connector.connect(user='root',password='123456',database='test',use_unicode=True)
+cursor = conn.cursor()
+
+cursor.execute('create table user (id varchar(20) primary key,name varchar(20))')
+
+cursor.execute('insert into user (id,name) values (%s,%s)',['1','Michael'])
+cursor.rowcount
+
+conn.commit()
+cursor.close()
+
+conn.close()
